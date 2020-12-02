f0bb71d0b26ce0ffa320a0ca30eb657feb8e7584
Nathan <namluu253@gmail.com>
2020-12-02 15:34:23 +0700

    update bootstrap for the website

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ $(document).ready(function()
             var i;
             for(i=0; i<30; ++i)
                 {
-                    $("#dt" + i).append("<a href='" + data.img[i].news_source + "'><img src ='" + data.img[i].source + "'></a></br><a href='" + data.img[i].news_source + "'>" + data.img[i].caption + "</a>");
+                    $("#dt" + i).append("<a href='" + data.img[i].news_source + "'><img src ='" + data.img[i].source + "'></a>");
+                    $("#dtct" + i).append("<a href='" + data.img[i].news_source + "'>" + data.img[i].caption + "</a>");
                 }
         });
         $.getJSON("./image_tuoitre.json", function(data)
@@ -13,7 +14,8 @@ $(document).ready(function()
             var i;
             for(i=0; i<30; ++i)
                 {
-                    $("#tt" + i).append("<a href='" + data.img[i].news_source + "'><img src ='" + data.img[i].source + "'></a></br><a href='" + data.img[i].news_source + "'>" + data.img[i].caption + "</a>");
+                    $("#tt" + i).append("<a href='" + data.img[i].news_source + "'><img src ='" + data.img[i].source + "'></a>");
+                    $("#ttct" + i).append("<a href='" + data.img[i].news_source + "'>" + data.img[i].caption + "</a>");
                 }
         });
         $.getJSON("./image_vnexpress.json", function(data)
@@ -21,7 +23,8 @@ $(document).ready(function()
             var i;
             for(i=0; i<30; ++i)
                 {
-                    $("#ve" + i).append("<a href='" + data.img[i].news_source + "'><img src ='" + data.img[i].source + "'></a></br><a href='" + data.img[i].news_source + "'>" + data.img[i].caption + "</a>");
+                    $("#ve" + i).append("<a href='" + data.img[i].news_source + "'><img src ='" + data.img[i].source + "'></a>");
+                    $("#vect" + i).append("<a href='" + data.img[i].news_source + "'>" + data.img[i].caption + "</a>");
                 }
         });
 });
